2c9e0b50e76c9ef5926e8c1b92e162d9d1f893d6
Saksham Kumar Singh <Saksham@geniuslabs.live>
2024-03-11 00:17:57 +0530

    episode_inception

---
+++ App.js
@@ -0,0 +1,75 @@
+//In react while creating the element we always pass 3 argument (htmlTags , object , children) , where msg is the children . 2nd + 3rd argument is know as props
+// const heading = React.createElement(
+//     "h1",
+//     {id: "heading"},                                                     //props
+//     "Welcome to the world of React with the help of Akshay !!!!"        //props  and also children
+//   );
+//   console.log(heading)  //object
+//   const root = ReactDOM.createRoot(document.getElementById("root"));
+//   root.render(heading);
+
+
+
+/**
+ *  <div id ="parent">
+ *       <div id = "child">
+ *           <h1> i am child </h1>
+ *      </div>
+ * </div>
+ * 
+ */ 
+
+
+// const divparent = React.createElement("div", {id:"parent"},
+// React.createElement("div", {id:"child"},
+// React.createElement("h1", {},"i am child")));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(divparent);
+
+
+
+/**
+ *  <div id ="parent">
+ *       <div id = "child">
+ *           <h1> i am child </h1>
+ *           <h2> i am child 2 </h2>
+ *      </div>
+ * </div>
+ * 
+ */ 
+
+
+// const divparent = React.createElement("div", {id:"parent"},
+// React.createElement("div", {id:"child"},
+// [React.createElement("h1", {}, "i am child") , 
+// React.createElement("h2", {}, "in am child 2")] ) );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(divparent); 
+
+
+
+/**
+ *  <div id ="parent">
+ *       <div id = "child">
+ *           <h1> i am child </h1>
+ *           <h2> i am child 2 </h2>
+ *      </div>
+ *      <div id = "child2">
+ *           <h3> i am child 3 </h1>
+ *           <h4> i am child 4 </h2>
+ *      </div>
+ * </div>
+ * 
+ */ 
+
+const divparent = React.createElement("div", {id: "parent"}, 
+[React.createElement("div", {id: "child"},
+[React.createElement("h1", {}, "i am child"),
+React.createElement("h2", {}, "i am child 2")]),
+React.createElement("div", {id:"child2"},
+[React.createElement("h3", {}, "i am child 3"),
+React.createElement("h4", {}, "i am child 4")])])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(divparent);
